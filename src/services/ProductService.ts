dbaaa55b71c7445aaa3984049bda7c3977d38b62
Layla Rodas <rodas.layla@gmail.com>
2024-04-27 10:04:19 +0200

    add types/schemas --- valibot

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,8 +1,18 @@
+import { safeParse } from "valibot";
+import { DraftProductSchema } from "../types"
 
 type ProductData = {
   [k: string]: FormDataEntryValue;
 }
 
 export async function addProduct( data : ProductData) {
-  console.log(data)
+  try {
+    const result = safeParse(DraftProductSchema, {
+      name: data.name,
+      price: +data.price
+    })
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
 }
